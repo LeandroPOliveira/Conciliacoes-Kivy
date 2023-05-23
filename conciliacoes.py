082dea9eadb4854d1c38a929759cf19063ad40c8
LeandroPOliveira <leandro_lpo2@hotmail.com>
2023-05-23 17:05:16 -0300

    ajustes gerais - mudança Sharepoint

--- conciliacoes.py
+++ conciliacoes.py
@@ -3,6 +3,7 @@ import threading
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 from kivy.clock import Clock
+from kivy.core.window import Window
 from kivy.properties import StringProperty, ListProperty, BooleanProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
@@ -13,7 +14,6 @@ from kivy.utils import get_color_from_hex
 import getpass
 from kivymd.app import MDApp
 from kivymd.uix.datatables import MDDataTable
-from kivy.lang.builder import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.metrics import dp
 import numpy as np
@@ -32,13 +32,13 @@ class TelaLogin(Screen):
         super().__init__(**kwargs)
         self.menu = None
         self.dialog = None
-        self.dicio = {}
+        self.nome_usuario = {}
         self.lista_usuarios = []
         with open('usuarios.txt') as users:
             self.usuarios = users.readlines()
             for user in self.usuarios:
                 self.lista_usuarios.append(user.split(';')[0])
-                self.dicio.update({user.split(';')[0]: user.split(';')[1]})
+                self.nome_usuario.update({user.split(';')[0]: user.split(';')[1]})
 
     def abre_menu(self):
         menu_items = [{"viewclass": "OneLineListItem", "text": i, "height": dp(56), "on_release":
@@ -52,7 +52,7 @@ class TelaLogin(Screen):
         self.menu.dismiss()
 
     def verifica_usuario(self):
-        if self.dicio[self.ids.drop_item.text].strip() == getpass.getuser():
+        if self.nome_usuario[self.ids.drop_item.text].strip() == getpass.getuser():
             self.manager.current = 'validar'
         else:
             self.dialog = MDDialog(text="Usuário inválido!", radius=[20, 7, 20, 7], )
@@ -71,45 +71,51 @@ class TelaValidacao(Screen):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.caminho_mes = None
         self.dialog = None
-        self.lista = []
+        self.intervalo_meses = []
         self.validos = []
 
         for i in range(12):
             mes = datetime.today()
             data_limite = mes - relativedelta(months=i)
-            self.lista.append(data_limite.strftime('%m.%Y'))
+            self.intervalo_meses.append(data_limite.strftime('%m.%Y'))
 
     def mes_selecionado(self, text):
-        self.text = text
+        self.mes_atual = text
         # Arquivo que serve como base para identificar a pasta de trabalho a ser utilizada, o ano é alterado de acordo
         # com a competencia selecionada pelo usuário
         with open('pasta.txt', 'r', encoding='utf-8') as path:
-            self.caminho = path.readlines()
-            self.caminho = [''.join((self.caminho[i][:45], self.text[3:9], self.caminho[i][49:])).rstrip() for i, row in
-                            enumerate(self.caminho)]
-        with open('pasta.txt', 'w', encoding='utf-8') as path:
-            for ano in self.caminho:
-                path.write(f'{ano}\n')
+            self.dados = path.readlines()
 
-        return self.text, self.caminho
+            self.caminho = os.path.join(self.dados[0].strip(), getpass.getuser(),
+                                        self.dados[1][0:85] + '_' + self.mes_atual[3:9], self.dados[1][93:].rstrip())
+
+            self.caminho_balancete = os.path.join(self.caminho, 'BALANCETES\SOCIETÁRIOS')
+
+
+        # with open('pasta.txt', 'w', encoding='utf-8') as path:
+        #     path.write(self.dados[0])
+        #     path.write(f'{self.caminho[]}\n')
+
+        return self.mes_atual, self.caminho
 
     def status(self):  # Verifica se a situação das conciliações, se está validada ou pendente
         self.validos.clear()
-        with open(os.path.join(*self.caminho[0].split('\\')[:3], 'dados.txt'), 'r') as f:
+        with open('dados.txt', 'r') as f:
             lines = f.readlines()[1:]
             self.meu_status, self.meu_status1, self.meu_status2 = '', '', ''
             for i in lines:  # verifica a situação da competência selecionada, usuarios que validaram ou não
                 i = i.split(';')
-                if i[0] == self.text and i[1] == self.manager.get_screen('login').lista_usuarios[1] \
+                if i[0] == self.mes_atual and i[1] == self.manager.get_screen('login').lista_usuarios[1] \
                         and i[2].strip() == 'OK':
                     self.meu_status, self.icone, self.cor = 'Validado', 'check-circle', [0, 1, 0, 1]
                     self.validos.append('OK')
-                elif i[0] == self.text and i[1] == self.manager.get_screen('login').lista_usuarios[2] \
+                elif i[0] == self.mes_atual and i[1] == self.manager.get_screen('login').lista_usuarios[2] \
                         and i[2].strip() == 'OK':
                     self.meu_status1, self.icone1, self.cor1 = 'Validado', 'check-circle', [0, 1, 0, 1]
                     self.validos.append('OK')
-                elif i[0] == self.text and i[1] == self.manager.get_screen('login').lista_usuarios[3] \
+                elif i[0] == self.mes_atual and i[1] == self.manager.get_screen('login').lista_usuarios[3] \
                         and i[2].strip() == 'OK':
                     self.meu_status2, self.icone2, self.cor2 = 'Validado', 'check-circle', [0, 1, 0, 1]
                     self.validos.append('OK')
@@ -145,7 +151,7 @@ class TelaValidacao(Screen):
             Clock.unschedule(self.check_foo_thread)
 
     def assina_gestor(self):
-        self.caminho_mes = os.path.join(self.caminho[0], self.ids.spinner_id2.text)
+        self.caminho_mes = os.path.join(self.caminho, self.ids.spinner_id2.text)
         # Criar pdf com assinatura
         c = canvas.Canvas('watermark.pdf')
         # posicionar a imagem da assinatura nas coordenadas x e y
@@ -197,7 +203,7 @@ class TelaRelatorio(Screen):
                 self.lista_usuarios.append(u.split(';')[0].strip())
 
     def validacao(self):
-        self.caminho_mes = os.path.join(self.manager.get_screen('validar').caminho[0],
+        self.caminho_mes = os.path.join(self.manager.get_screen('validar').caminho,
                                         self.manager.get_screen('validar').ids.spinner_id2.text)
 
         pasta1 = os.listdir(self.caminho_mes)
@@ -207,7 +213,9 @@ class TelaRelatorio(Screen):
                 pasta1.remove(i)
 
         for i in pasta1:
+            print(i)
             if i.startswith('Conta') or i.startswith('conta'):
+
                 wb = openpyxl.load_workbook(os.path.join(self.caminho_mes, i),
                                             read_only=True)
                 sheets = wb.sheetnames
@@ -231,7 +239,7 @@ class TelaRelatorio(Screen):
         self.data = pd.DataFrame(lista).T
         self.data.columns = ['Conta', 'Data', 'Debito', 'Credito', 'Balancete']
         # Listar planilhas dos balancetes
-        self.pasta_balancetes = self.manager.get_screen('validar').caminho[1]
+        self.pasta_balancetes = self.manager.get_screen('validar').caminho_balancete
 
         lista = os.listdir(self.pasta_balancetes)
 
@@ -302,12 +310,17 @@ class TelaRelatorio(Screen):
                                   current_row[0].replace('.', '') + '.xlsx'))
 
     def start_foo_thread2(self):
-        self.foo_thread = threading.Thread(target=self.assina)
-        self.foo_thread.daemon = True
-        self.pb = MDDialog(text="Aguarde...", radius=[20, 7, 20, 7], )
-        self.pb.open()
-        self.foo_thread.start()
-        Clock.schedule_interval(self.check_foo_thread2, 10)
+        valida = self.data['Status'].unique()
+        if 'OK' in valida and len(valida) == 1:  # checa se não existem contas que não estão validadas
+            self.foo_thread = threading.Thread(target=self.assina)
+            self.foo_thread.daemon = True
+            self.pb = MDDialog(text="Aguarde...", radius=[20, 7, 20, 7], )
+            self.pb.open()
+            self.foo_thread.start()
+            Clock.schedule_interval(self.check_foo_thread2, 10)
+        else:
+            self.dialog_erro = MDDialog(text="Erro! Verificar pendências!", radius=[20, 7, 20, 7], )
+            self.dialog_erro.open()
 
     def check_foo_thread2(self, dt):
         if self.foo_thread.is_alive():
@@ -318,75 +331,68 @@ class TelaRelatorio(Screen):
             self.dialog.open()
             Clock.unschedule(self.check_foo_thread2)
 
-
     def assina(self):
-        valida = self.data['Status'].unique()
-        if 'OK' in valida and len(valida) == 1:  # checa se não existem contas que não estão validadas
-            lista3 = []
-            for i in self.data['Conta']:
-                conta = 'Conta ' + i.replace('.', '') + '.xlsx'
-                lista3.append(conta)
+        lista3 = []
+        for i in self.data['Conta']:
+            conta = 'Conta ' + i.replace('.', '') + '.xlsx'
+            lista3.append(conta)
 
-            # Criar pdf com assinatura
-            c = canvas.Canvas('watermark.pdf')
-            # posicionar a imagem da assinatura nas coordenadas x e y
-            c.drawImage(self.manager.get_screen('login').ids.drop_item.text + '.png', 40, 50, 150, 100,
-                        mask='auto')
-            c.save()
+        # Criar pdf com assinatura
+        c = canvas.Canvas('watermark.pdf')
+        # posicionar a imagem da assinatura nas coordenadas x e y
+        c.drawImage(self.manager.get_screen('login').ids.drop_item.text + '.png', 40, 50, 150, 100,
+                    mask='auto')
+        c.save()
 
-            for i in lista3:  # converter arquivos excel para pdf
-                excel = client.Dispatch("Excel.Application")
-                sheets = excel.Workbooks.Open(self.caminho_mes + '\\' + i)
-                work_sheets = sheets.Worksheets[1]
-                path = os.path.join(self.caminho_mes, 'teste ' + i.replace('.xlsx', '.pdf'))
-                work_sheets.ExportAsFixedFormat(0, path)
+        for i in lista3:  # converter arquivos excel para pdf
+            excel = client.Dispatch("Excel.Application")
+            sheets = excel.Workbooks.Open(self.caminho_mes + '\\' + i)
+            work_sheets = sheets.Worksheets[1]
+            path = os.path.join(self.caminho_mes, 'teste ' + i.replace('.xlsx', '.pdf'))
+            work_sheets.ExportAsFixedFormat(0, path)
 
-                # Buscar a assinatura criada
-                watermark = PdfFileReader(open("watermark.pdf", "rb"))
-                output = PdfFileWriter()
+            # Buscar a assinatura criada
+            watermark = PdfFileReader(open("watermark.pdf", "rb"))
+            output = PdfFileWriter()
 
-                with open(path, "rb") as provisorio:
-                    arquivo = PdfFileReader(provisorio)
-                    number_of_pages = arquivo.getNumPages()
+            with open(path, "rb") as provisorio:
+                arquivo = PdfFileReader(provisorio)
+                number_of_pages = arquivo.getNumPages()
 
-                    for current_page_number in range(number_of_pages):
-                        page = arquivo.getPage(current_page_number)
-                        if page.extractText() != "":
-                            output.addPage(page)
+                for current_page_number in range(number_of_pages):
+                    page = arquivo.getPage(current_page_number)
+                    if page.extractText() != "":
+                        output.addPage(page)
 
-                    page_count = output.getNumPages()
-                    output2 = PdfFileWriter()
-                    # percorrer o arquivo para unir o pdf de assinatura na última pagina do arquivo principal
-                    for page_number in range(page_count):
-                        input_page = output.getPage(page_number)
-                        if page_number == page_count - 1:
-                            input_page.mergePage(watermark.getPage(0))
-                        output2.addPage(input_page)
+                page_count = output.getNumPages()
+                output2 = PdfFileWriter()
+                # percorrer o arquivo para unir o pdf de assinatura na última pagina do arquivo principal
+                for page_number in range(page_count):
+                    input_page = output.getPage(page_number)
+                    if page_number == page_count - 1:
+                        input_page.mergePage(watermark.getPage(0))
+                    output2.addPage(input_page)
 
-                    # exportar o arquivo pdf com o nome sufixo pendente, que aguardará assinatura do gestor
-                    with open(os.path.join(self.caminho_mes, 'pendente' +
-                                                             i.replace('.xlsx', '.pdf')), "wb") as outputStream:
-                        output2.write(outputStream)
+                # exportar o arquivo pdf com o nome sufixo pendente, que aguardará assinatura do gestor
+                with open(os.path.join(self.caminho_mes, 'pendente' +
+                                                         i.replace('.xlsx', '.pdf')), "wb") as outputStream:
+                    output2.write(outputStream)
 
-                os.remove(path)
-                sheets.Close(True)
+            os.remove(path)
+            sheets.Close(True)
 
-            #  Após assinatura, criar um log simples em um arquivo de texto que permitirá ao sistema verificar o
-            #  usuario que validou a sua respectiva conciliação e o período
-            adicionar = [self.manager.get_screen('validar').ids.spinner_id2.text,
-                         self.manager.get_screen('login').ids.drop_item.text, 'OK']
-            adicionar = ';'.join(adicionar)
-            # salvar observação no arquivo
-            with open(os.path.join(*self.manager.get_screen('validar').caminho[0].split('\\')[:3], 'dados.txt'),
-                      'a') as f:
-                f.write(f'\n{adicionar}')
+        #  Após assinatura, criar um log simples em um arquivo de texto que permitirá ao sistema verificar o
+        #  usuario que validou a sua respectiva conciliação e o período
+        adicionar = [self.manager.get_screen('validar').ids.spinner_id2.text,
+                     self.manager.get_screen('login').ids.drop_item.text, 'OK']
+        adicionar = ';'.join(adicionar)
+        # salvar observação no arquivo
+        with open(os.path.join('dados.txt'),
+                  'a') as f:
+            f.write(f'\n{adicionar}')
 
-            self.dialog_ok = MDDialog(text="Assinado com sucesso!", radius=[20, 7, 20, 7], )
-            self.dialog_ok.open()
-
-        else:
-            self.dialog_erro = MDDialog(text="Erro! Verificar pendências!", radius=[20, 7, 20, 7], )
-            self.dialog_erro.open()
+        self.dialog_ok = MDDialog(text="Assinado com sucesso!", radius=[20, 7, 20, 7], )
+        self.dialog_ok.open()
 
 
 class Content(BoxLayout):
@@ -470,6 +476,7 @@ class WindowManager(ScreenManager):
 
 
 class Conciliacoes(MDApp):
+    Window.maximize()
     popupWindow = None
 
     def meu_popup(self):
@@ -480,7 +487,6 @@ class Conciliacoes(MDApp):
 
     def build(self):
         pass
-        # return Builder.load_file('conciliacoes.kv')
 
 
 Conciliacoes().run()
